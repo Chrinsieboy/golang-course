8c943bc01a37dd6aae3aec7a970b281995275bd5
Chris Friemann <github@chris.friemann.nl>
2024-01-13 21:28:10 +0100

    feat: add data types for user input

--- main.go
+++ main.go
@@ -10,4 +10,12 @@ func main() {
 	fmt.Println("Welcome to", conferenceName, "booking application!")
 	fmt.Println("We have a total of", confrenceTickets, "tickets and", remainingTickets, "are still available")
 	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+	// ask user for name
+
+	userName = "Chris"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
 }
