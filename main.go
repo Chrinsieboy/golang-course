341dd2356a0ab560f92cd7245ccd203389d89b25
Chris Friemann <github@chris.friemann.nl>
2024-01-13 17:18:50 +0100

    feat: implemented the variables

--- main.go
+++ main.go
@@ -7,5 +7,7 @@ func main() {
 	const confrenceTickets = 50
 	var remainingTickets = 50
 
+	fmt.Println("Welcome to", conferenceName, "booking application!")
+	fmt.Println("We have a total of", confrenceTickets, "tickets and", remainingTickets, "are still available")
 	fmt.Println("Get your tickets here to attend")
 }
